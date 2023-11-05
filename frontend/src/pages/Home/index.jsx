a6b41be3f54286d47140865f25d6ad2c1d1479c7
Matheus Ferreira <60938127+matheushenriqueferreira@users.noreply.github.com>
2023-11-05 15:53:27 -0300

    react-router-dom, Homepage route

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,38 +1,9 @@
 const Home = () => {
-  const [ola, setOla] = useState("");
-  const [users, setUsers] = useState([]);
-  
-  const t = [];
-
-  const getTeste = async () => {
-    const response = await fetch("http://localhost:3000");
-    const text = await response.text();
-    setOla(text);
-  }
-
-  const getUsers = async () => {
-    const response = await fetch("http://localhost:3000/user");
-    const userList = await response.json();
-    userList.users.forEach(element => {
-      t.push(element)
-    });
-    setUsers(t);
-  }
-
-  useEffect(() => {
-    getTeste();
-    getUsers();
-  },[]);
-  
   return(
-    <>
-      <p>{ola}</p>
-      <ul>
-        {
-          users.map((item) => <li key={item.id}>{item.email}</li>)
-        }
-      </ul>
-        
+    <> 
+      <main>
+        <h1>Home</h1>
+      </main>
     </>
   );
 }
