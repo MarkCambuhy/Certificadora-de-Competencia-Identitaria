4fdc5a41599dd3d17d67a91352e16f4b6d42629d
Matheus Ferreira <60938127+matheushenriqueferreira@users.noreply.github.com>
2023-11-11 19:39:34 -0300

    Return message

---
+++ backend/src/services/UserService.js
@@ -0,0 +1,34 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+
+const prisma = new PrismaClient();
+
+class UserService {
+  static async insert(user) {
+    const { firstName, lastName, email, password } = user;
+    try {
+      const userExists = await prisma.user.findUnique({ where: { email } });
+    
+      if(userExists === null) {
+        const encryptedPassword = await bcrypt.hash(password, 10);
+        const newUser = await prisma.user.create({
+          data: {
+            firstName,
+            lastName,
+            email,
+            password: encryptedPassword,
+          },
+        });
+        return "Usuário criado com sucesso!";
+      }
+      else {
+        return "Usuário já existe!";
+      }
+    }
+    catch (error) {
+      return error;
+    }
+  }
+}
+
+export { UserService }
