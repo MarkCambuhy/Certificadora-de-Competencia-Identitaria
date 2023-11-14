5c03ea2562b0d66434f38a7e687a536647bc1a6a
Matheus Ferreira <60938127+matheushenriqueferreira@users.noreply.github.com>
2023-11-13 21:52:09 -0300

    Registration page design

--- frontend/src/pages/Registration/index.jsx
+++ frontend/src/pages/Registration/index.jsx
@@ -1,13 +1,12 @@
 import { useEffect, useState } from "react";
 import "./index.css";
-import logo from "../../assets/bons-fluidos-110px.png";
+import logo from "../../assets/bons-fluidos-30px.png";
 
 const Registration = () => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [repeatPassword, setRepeatPassword] = useState("");
   const [message, setMessage] = useState("");
 
   const handlingRegistration = async (evt) => {
@@ -21,36 +20,38 @@ const Registration = () => {
   return(
     <>
       <main className="registrationStyle">
-        <section>
-          <div className="logoContainer">
-            <img src={logo} alt="Logo Bons fluidos" />
+        <section className="sectionBg">
+        </section>
+        <section className="dataSection">
+          <div>
+            <div className="registrationLogoContainer">
+              <img src={logo} alt="" />
+              <p>Bons fluidos</p>
+            </div>
+            <h1 className="registrationTitle">Cadastre-se</h1>
           </div>
           <form method="POST" action="/signin/identifier" className="formContainer">
             <div>
-              <label htmlFor="firstName">Nome:</label>
-              <input type="text" id="firstName" value={firstName} onChange={(evt) => setFirstName(evt.target.value)} placeholder="Insira o primeiro nome" />
+              <label htmlFor="firstName">Nome</label>
+              <input type="text" id="firstName" value={firstName} onChange={(evt) => setFirstName(evt.target.value)} />
             </div>
             <div>
-              <label htmlFor="lastName">Sobrenome:</label>
-              <input type="text" id="lastName" value={lastName} onChange={(evt) => setLastName(evt.target.value)} placeholder="Insira o sobrenome" />
+              <label htmlFor="lastName">Sobrenome</label>
+              <input type="text" id="lastName" value={lastName} onChange={(evt) => setLastName(evt.target.value)} />
             </div>
             <div>
-              <label htmlFor="email">E-mail:</label>
-              <input type="email" id="email" value={email} onChange={(evt) => setEmail(evt.target.value)} placeholder="Insira seu e-mail" />
+              <label htmlFor="email">E-mail</label>
+              <input type="email" id="email" value={email} onChange={(evt) => setEmail(evt.target.value)} />
             </div>
             <div>
-              <label htmlFor="password">Senha:</label>
-              <input type="password" id="password" value={password} onChange={(evt) => setPassword(evt.target.value)} placeholder="Insira a sua senha" />
+              <label htmlFor="password">Senha</label>
+              <input type="password" id="password" value={password} onChange={(evt) => setPassword(evt.target.value)} />
+            </div>
+            <div className="btnRegisterContainer">
+              <button type="submit" onClick={(evt) => handlingRegistration(evt)} className="btnRegisterStyle">Cadastre-se</button>
             </div>
             <div>
-              <label htmlFor="repeatPassword">Repetir senha:</label>
-              <input type="password" id="repeatPassword" value={repeatPassword} onChange={(evt) => setRepeatPassword(evt.target.value)} placeholder="Repita a senha" />
-            </div>
-            <div>
-              <button type="submit" onClick={(evt) => handlingRegistration(evt)} className="btnRegisterStyle">Registrar</button>
-            </div>
-            <div>
-            <p className="registrationMessage">{message}</p>
+              <p className="registrationMessage">{message}</p>
             </div>
           </form>
         </section>
