bfea9e1646148248a3d292cd0c927e06bc8019d6
Rafaaaugg <rafaelmendes@alunos.utfpr.edu.br>
2023-11-23 09:33:12 -0300

    main

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,9 +1,40 @@
+import React, { useState, useEffect } from "react";
+import "./index.css";
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
+import Postagens from "../../components/posts/Posts";
+import logo from "../../assets/bons-fluidos-menu-bg.jpg";
+
+const posts = [
+  {
+   _id:1,
+   title:"NOTICIA",
+   content: "TEADASKFASFAS"
+  },
+  {
+    _id:2,
+    title:"NOTICIA",
+    content: "teste 1 2 3"
+  }
+]
+
 const Home = () => {
   return(
-    <> 
-      <main>
-        <h1>Home</h1>
-      </main>
+    <>
+    <Header />
+    <div className="container">
+      <div className="content">
+        <div className="background">
+          <img src={logo}/>
+        </div>
+        <div className="noticias">
+          <p>Noticias</p>
+          <hr></hr>
+        </div>
+        <Postagens posts={posts}/>
+      </div>
+    </div>
+    <Footer />
     </>
   );
 }
